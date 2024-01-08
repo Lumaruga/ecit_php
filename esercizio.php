60241f0e03f38aa50d54bd416f37f86b96e783a1
Andrea <andreadiblasi904@gmail.com>
2024-01-08 15:49:15 +0100

    finito(?)

--- esercizio.php
+++ esercizio.php
@@ -7,18 +7,23 @@
     </form>
     
     <?php
-        echo"<h2>UTILIZZO DATI INSERITI DALL'UTENTE<br></h2>";
-       if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $nome = $_POST["nome"];
-        echo "Nome: $nome <br>";
+        echo "<h2>UTILIZZO DATI INSERITI DALL'UTENTE<br></h2>";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nome = $_POST["nome"];
+            if (preg_match("/^[a-zA-Z]+$/", $nome)) {
+                echo "Nome: $nome <br>";
+            } else {
+                $error = "ERRORE: Il nome deve contenere solo lettere dell'alfabeto.";
+                echo $error;
+            }
         }
 
-        echo"<h2>CICLO CHE STAMPA I NUMERI DA 0 A 10<br></h2>";
+        echo "<h2>CICLO CHE STAMPA I NUMERI DA 0 A 10<br></h2>";
         for ($i = 1; $i <= 10; $i++) {
             echo $i . " ";
         }
         
-        echo"<h2>ARRAY ASSOCIATIVO<br></h2>";
+        echo "<h2>ARRAY ASSOCIATIVO<br></h2>";
         $array = [
             'nome' => 'Mario', 
             'cognome' => 'Verdi', 
