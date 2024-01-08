702f6507d0a3fc9e10d584f46783054820e16ebf
Andrea <andreadiblasi904@gmail.com>
2024-01-08 15:06:56 +0100

    finito(?)

--- esercizio.php
+++ esercizio.php
@@ -7,18 +7,18 @@
     </form>
     
     <?php
-        echo"UTILIZZO DATI INSERITI DALL'UTENTE<br>";
+        echo"<h2>UTILIZZO DATI INSERITI DALL'UTENTE<br></h2>";
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $nome = $_POST["nome"];
         echo "Nome: $nome <br>";
         }
 
-        echo"CICLO CHE STAMPA I NUMERI DA 0 A 10<br>";
+        echo"<h2>CICLO CHE STAMPA I NUMERI DA 0 A 10<br></h2>";
         for ($i = 1; $i <= 10; $i++) {
             echo $i . " ";
         }
         
-        echo"ARRAY ASSOCIATIVO<br>";
+        echo"<h2>ARRAY ASSOCIATIVO<br></h2>";
         $array = [
             'nome' => 'Mario', 
             'cognome' => 'Verdi', 
